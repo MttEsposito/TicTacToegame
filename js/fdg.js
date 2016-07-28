c5350c52b36ce09ef8b4cd1288777d76bb66ba33
MttEsposito <mattiaespo.me@gmail.com>
2016-07-28 08:51:11 +0000

    Commit version 1.8 adding score point player and background

--- js/fdg.js
+++ js/fdg.js
@@ -1,32 +1,34 @@
 //Initialization of variabilies
-var image = new Array ("img/0.png", "img/1.png", "img/2.png")
-var d = new Array ("0", "0", "0", "0", "0", "0", "0", "0", "0")
-var x = 1
-var f = 0
+var image = new Array ("img/0.png", "img/1.png", "img/2.png");
+var d = new Array ("0", "0", "0", "0", "0", "0", "0", "0", "0");
+var x = 1;
+var f = 0;
+var pl1v = 0;
+var pl2v = 0;
 
 //Changed the name in the label as the turn of the player
 function name (a) {
-  if (a == 1) return document.f1.player1.value
-  else return document.f1.player2.value
+  if (a == 1) return document.f1.player1.value;
+  else return document.f1.player2.value;
 }
 
 //function of the insert X or O and call of function check
 function playIn (n) {
 	var display = document.getElementById("display");
 	if ((d[n] == 0) && (f == 0)) {
-    document.images[n + 2].src = image[x]
+    document.images[n + 2].src = image[x];
     if (x == 1) {
     	display.setAttribute("class","gg1");
-      document.f1.campo.value = "Turno di " + name(2)
-      d[n] = 1
-      check()
-      x = 2
+      document.f1.result.value = "Turno di " + name(2);
+      d[n] = 1;
+      check();
+      x = 2;
     } else {
     	display.setAttribute("class","gg2");
-      document.f1.campo.value = "Turno di " + name(1)
-      d[n] = 2
-      check()
-      x = 1
+      document.f1.result.value = "Turno di " + name(1);
+      d[n] = 2;
+      check();
+      x = 1;
     }
   }
 }
@@ -37,7 +39,7 @@ function play(){
        audio.play();
 }
 
-//function check all grid for the winner player
+//function check all grid for the winner player and point assignment
 function check() {
   if (((d[0] == x)&&(d[1] == x)&&(d[2] == x)) ||
       ((d[3] == x)&&(d[4] == x)&&(d[5] == x)) ||
@@ -47,18 +49,26 @@ function check() {
       ((d[2] == x)&&(d[5] == x)&&(d[8] == x)) ||
       ((d[0] == x)&&(d[4] == x)&&(d[8] == x)) ||
       ((d[2] == x)&&(d[4] == x)&&(d[6] == x))) {
-      f = 1
+      f = 1;
   	display.setAttribute("class","win");
-     document.f1.campo.value = "Vincitore : " + name(x)
+     document.f1.result.value = "Vincitore : " + name(x);
      document.getElementById("bnewgame").disabled = false;
+     if (x == 1){
+      pl1v = pl1v + 1;
+      document.f1.scorepl1.value = pl1v;
+      }else{
+        pl2v=pl2v + 1;
+        document.f1.scorepl2.value = pl2v;
+      }
+      
   }
   if (f == 0) {
-    var p = 0
+    var p = 0;
     document.getElementById("bnewgame").disabled = true;
-    for (var i = 0; i < 9; i++) if (d[i] != 0) p++
+    for (var i = 0; i < 9; i++) if (d[i] != 0) p++;
   if (p == 9){ 
   	display.setAttribute("class","win");
-	document.f1.campo.value = "Parità";
+	document.f1.result.value = "Parità";
 	document.getElementById("bnewgame").disabled = false;
 }
   }
@@ -77,11 +87,11 @@ function muted(param){
   var dibutton = document.getElementById("disableaudio");
   if (!param){
     dibutton.setAttribute("hidden", true);
-    enbutton.removeAttribute("hidden")
+    enbutton.removeAttribute("hidden");
   }else
   {
       enbutton.setAttribute("hidden", true);
-      dibutton.removeAttribute("hidden")
+      dibutton.removeAttribute("hidden");
       
   }
 }
